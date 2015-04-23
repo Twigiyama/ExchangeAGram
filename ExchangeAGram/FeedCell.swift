5ed039d4b254b9ed1c435f45f9c50d04ce052705
Twigiyama <asitha@tomorrows.company>
2015-04-23 11:28:11 +0100

    Adding CollectionCellViewCell class for prototype cell.

---
+++ ExchangeAGram/FeedCell.swift
@@ -0,0 +1,15 @@
+//
+//  FeedCell.swift
+//  ExchangeAGram
+//
+//  Created by Asitha Rodrigo on 23/04/2015.
+//  Copyright (c) 2015 Twig. All rights reserved.
+//
+
+import UIKit
+
+class FeedCell: UICollectionViewCell {
+    
+    
+    
+}
